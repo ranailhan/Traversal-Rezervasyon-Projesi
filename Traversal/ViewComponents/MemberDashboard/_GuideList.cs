a73e0d35a39a13a02606d62ec5fd96b240ec0ef1
ranailhan <rana.ilhan05@gmail.com>
2026-01-12 21:02:00 +0300

    Add tour guide list view component to member dashboard

---
+++ Traversal/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {  
+            return View(); 
+        }
+    }
+}
