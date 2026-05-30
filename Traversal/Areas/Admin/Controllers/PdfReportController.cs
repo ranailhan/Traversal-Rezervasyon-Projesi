88d0cabb55fc4f21f26345bb1c464e057a8f35af
ranailhan <rana.ilhan05@gmail.com>
2026-05-30 20:01:36 +0300

    Implement Pdf Reports part 1

---
+++ Traversal/Areas/Admin/Controllers/PdfReportController.cs
@@ -0,0 +1,63 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StaticPdfReport() 
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/PdfReports/"+"dosya1.pdf");
+            var stream = new FileStream(path,FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document,stream);
+
+            document.Open();
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+            return File("/PdfReports/dosya1.pdf", "application/pdf", "dosya2.pdf");
+            
+        }
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PdfReports/" + "dosya2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+            PdfPTable pdfPTable = new PdfPTable(3);
+            pdfPTable.AddCell("Misafir Adı");
+            pdfPTable.AddCell("Misafir Soyadı");
+            pdfPTable.AddCell("Misafir Tc");
+
+            pdfPTable.AddCell("Eylül");
+            pdfPTable.AddCell("Yücedağ");
+            pdfPTable.AddCell("12345678911");
+
+            pdfPTable.AddCell("Kemal");
+            pdfPTable.AddCell("Yıldırım");
+            pdfPTable.AddCell("98765432145");
+
+            pdfPTable.AddCell("Ali");
+            pdfPTable.AddCell("Cin");
+            pdfPTable.AddCell("56425789641");
+
+            document.Add(pdfPTable);
+
+            document.Close();
+            return File("/PdfReports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
+    }
+}
