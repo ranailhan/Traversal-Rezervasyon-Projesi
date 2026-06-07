9d79737ef7f0dab7914a4321476dc89ac5d5429d
ranailhan <rana.ilhan05@gmail.com>
2026-06-07 23:17:32 +0300

    add mail sending functionality

---
+++ Traversal/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,53 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Traversal.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "testintestideneme@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.MailReceiver);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587);
+            client.Authenticate(
+                            _configuration["MailSettings:Mail"],
+                            _configuration["MailSettings:Password"]
+                        ); client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Traversal/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Traversal.Models
+{
+    public class MailRequest
+    {
+        public string SenderMail { get; set; }
+        public string SenderName { get; set; }
+        public string MailReceiver { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
