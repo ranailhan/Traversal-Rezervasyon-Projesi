31d2e54910fa2ad7c7b9ed348f51e73658448180
ranailhan <rana.ilhan05@gmail.com>
2026-06-10 21:00:25 +0300

    ajax part 1

---
+++ Traversal/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Traversal.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityId = 1,
+                CityName="Üsküp",
+                CityCountry="Makedonya"
+            },
+            new CityClass
+            {
+                CityId = 2,
+                CityName="Roma",
+                CityCountry="İtalya"
+            },
+            new CityClass
+            {
+               CityId=3,
+               CityName="Londra",
+               CityCountry="İngiltere"
+            }
+        };
+    }
+}
